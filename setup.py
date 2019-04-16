5718cbf1141d12a6660fc177f54c85a2a5dee167
florimondmanca <florimond.manca@gmail.com>
2019-04-17 00:52:33 +0200

    update setup.py script

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import re
 import setuptools
 
 description = "Standard development tooling for Bocadillo"
@@ -5,12 +7,25 @@ description = "Standard development tooling for Bocadillo"
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
+NAME = "bocadillo-cli"
+PACKAGE = "bocadillo_cli"
 GITHUB = "https://github.com/bocadilloproject/bocadillo-cli"
 CHANGELOG = f"{GITHUB}/blob/master/CHANGELOG.md"
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def find_version(*parts):
+    with open(os.path.join(HERE, *parts), "r") as fp:
+        content = fp.read()
+    version_match = re.match(r"^__version__ = ['\"]([^'\"]*)['\"]", content)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setuptools.setup(
-    name="bocadillo-cli",
-    version="0.0.1",
+    name=NAME,
+    version=find_version(PACKAGE, "version.py"),
     author="Florimond Manca",
     author_email="florimond.manca@gmail.com",
     description=description,
@@ -18,7 +33,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    entry_points={"console_scripts": ["bocadillo=main:cli"]},
+    entry_points={"console_scripts": ["bocadillo=bocadillo_cli.main:cli"]},
     install_requires=["click>=7.0, <8.0", "jinja2>=2.10.1"],
     python_requires=">=3.6",
     url=GITHUB,
